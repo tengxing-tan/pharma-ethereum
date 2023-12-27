c1db4e4c7f373384418673cc507fe7cde8c391d7
Tan Teng Xing <seventan1234@gmail.com>
2023-12-27 17:02:34 +0800

    Remove SearchBar component

--- next-app/app/(stakholder)/product-catalogue/page.tsx
+++ next-app/app/(stakholder)/product-catalogue/page.tsx
@@ -25,7 +25,6 @@ export default async function Page() {
             Create</button>
         </Link>
       </Heading>
-      <SearchBar />
 
       <p className="pt-4 text-sm text-gray-700">
         Showing {items.length} items.</p>
